6d2352f6e787236eb1ef9f439d1743315b526b28
luckarts <luc.bachelerieart@gmail.com>
2020-08-27 01:01:55 +0200

    fix form errors

--- server/Database/models/student.js
+++ server/Database/models/student.js
@@ -3,11 +3,18 @@ module.exports = (sequelize, DataTypes) => {
   const Student = sequelize.define(
     'Student',
     {
-      username: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      is: /^[A-Za-z]+$/i,
-      len: [2, 15]
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          len: {
+            arg: [2, 15],
+            message: 'username has to between 2 and 15 characters'
+          },
+          is: /^[A-Za-z]+$/i
+        }
+      }
     },
     {}
   );
